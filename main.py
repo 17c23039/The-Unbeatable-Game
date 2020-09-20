779cdaf9553c332597b41573c0387909fa42c171
Liam Dixon <liamadixon@icloud.com>
2020-09-20 09:06:59 +0000

    Rock paper scissors simple code finished.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from time import sleep
 import random
-score = int(0)
-global score
 def rps () :
   rpschoice = input("Rock, paper, or scissors? R/P/S. ")
   sleep(1)
@@ -13,32 +11,22 @@ def rps () :
   sleep(1)
   print("Shoot!")
   sleep(1)
-  aichoice = random.randint(1, 1)
+  aichoice = random.randint(1, 3)
   if aichoice == 1 :
     print("Rock!")
     sleep(1)
     if rpschoice == "R" :
       print("Tie!")
-      print(f"Your score is " + score + ".")
     elif rpschoice == "r" :
       print("Tie!")
-      print(f"Your score is " + score + ".")
     elif rpschoice == "P" :
       print("You win!")
-      score = score + 1
-      print(f"Your score is " + score + ".")
     elif rpschoice == "p" :
       print("You win!")
-      score = score + 1
-      print(f"Your score is " + score + ".")
     elif rpschoice == "S" :
       print("I win!")
-      score = score - 1
-      print(f"Your score is " + score + ".")
     elif rpschoice == "s" :
       print("I win!")
-      score = score - 1
-      print(f"Your score is " + score + ".")
     else :
       print("Invalid option! Restarting game...")
       sleep (2.478397498)
@@ -48,8 +36,39 @@ def rps () :
     sleep(1)
     if rpschoice == "R" :
       print("I win!")
-      score = score - 1
-      print(f"Your score is " + score + ".")
+    elif rpschoice == "r" :
+      print("I win!")
+    elif rpschoice == "P" :
+      print("Tie!")
+    elif rpschoice == "p" :
+      print("Tie!")
+    elif rpschoice == "S" :
+      print("You win!")
+    elif rpschoice == "s" :
+      print("You win!")
+    else :
+      print("Invalid option! Restarting game...")
+      sleep (2.478397498)
+      rps ()
+  elif aichoice == 3 :
+    print("Scissors!")
+    sleep(1)
+    if rpschoice == "R" :
+      print("You win!")
+    elif rpschoice == "r" :
+      print("You win!")
+    elif rpschoice == "P" :
+      print("I win!")
+    elif rpschoice == "p" :
+      print("I win!")
+    elif rpschoice == "S" :
+      print("Tie!")
+    elif rpschice == "s" :
+      print("Tie!")
+    else :
+      print("Invalid option! Restarting game...")
+      sleep (2.478397498)
+      rps ()
 print("Rock Paper Scissors, Python edition! This project is to see how complicated and advanced I can make a RPS game.")
 sleep(3)
 print("When it is your turn, you will have three options. [R]ock, [P]aper and [S]cissors! For a loss you will lose a point, a tie it will stay the same, and a win will add a point.")
